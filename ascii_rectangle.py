a142aa91882ea33e76dda4bb9c0c8ce7e52ad580
0xLysandre <gutsbuthalal@gmail.com>
2026-02-12 10:19:21 +0100

    yes

---
+++ ascii_rectangle.py
@@ -0,0 +1,47 @@
+from time import sleep
+from random import randint, choice
+
+class Cell:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.presence = False
+
+    def __str__(self):
+        if self.presence:
+            return "[x]"
+        return "[ ]"
+
+
+def delay_print(txt:str)->None:
+    for char in txt:
+        print(char,flush=True,end='')
+        sleep(0.0125)
+
+def ascii_rectangle():
+    SEP = "===="
+    MID_GAP = " " * 11
+
+    row0 = [Cell(0, c) for c in range(5)]
+    row1 = [Cell(1, 0), Cell(1, 1), None, Cell(1, 3), Cell(1, 4)]
+    row2 = [Cell(2, c) for c in range(5)]
+
+    full_line = lambda cells: SEP.join(str(c) for c in cells)
+    pipe_line = " |      |             |      |"
+
+    mid_line = (
+        str(row1[0]) + SEP + str(row1[1])
+        + MID_GAP
+        + str(row1[3]) + SEP + str(row1[4])
+    )
+
+    return (
+        full_line(row0) + "\n"
+        + pipe_line + "\n"
+        + mid_line + "\n"
+        + pipe_line + "\n"        
+        + full_line(row2) + "\n"
+    )
+
+if __name__ == "__main__":
+    delay_print(ascii_rectangle())
